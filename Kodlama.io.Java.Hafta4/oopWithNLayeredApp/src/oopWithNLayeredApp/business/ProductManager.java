a32c00c21b95972a283a886113bc6ce9fe6be423
mertbayraktar335 <mertbayraktar75@gmail.com>
2022-10-05 23:34:41 +0300

    week3

---
+++ Kodlama.io.Java.Hafta4/oopWithNLayeredApp/src/oopWithNLayeredApp/business/ProductManager.java
@@ -0,0 +1,29 @@
+package oopWithNLayeredApp.business;
+import oopWithNLayeredApp.core.logging.Logger;
+import oopWithNLayeredApp.dataAccess.ProductDao;
+import oopWithNLayeredApp.entities.Product;
+
+public class ProductManager {
+
+    private ProductDao productDao;
+    private Logger[] loggers;
+    
+    public ProductManager(ProductDao productDao,Logger[] loggers) {
+        this.productDao = productDao;
+        this.loggers=loggers;
+    }
+
+
+
+    public void add(Product product) throws Exception{
+        //iş kuralları
+        if(product.getUnitPrice()<10){
+            throw new Exception("10 TL altında olamaz");
+        }
+        productDao.add(product);
+
+        for (Logger logger : loggers) {
+            logger.log(product.getName());
+        }
+    }
+}
